2de7d8b2a8d9bd1e12537703539fa30d1a0ebe8d
sandeep-kadam <sandeep.kadam240@gmail.com>
2020-02-22 23:13:59 +0530

    spring core important topics

---
+++ springcore_app17_DependencyLookup-Problem/src/main/java/com/nt/beans/Engine.java
@@ -0,0 +1,17 @@
+package com.nt.beans;
+
+public class Engine {
+	
+	public void start(){
+		System.out.println("Engine started");
+   }
+	
+	public void stop(){
+		System.out.println("Engine stopped");
+	}
+	
+	public Engine() {
+		System.out.println("inside 0-param cons Engine");
+	}
+
+}
---
+++ springcore_app17_DependencyLookup-Problem/src/main/java/com/nt/beans/Viechle.java
@@ -0,0 +1,46 @@
+package com.nt.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Viechle {
+	private String beanId;
+
+	public void setBeanId(String beanId) {
+		this.beanId = beanId;
+	}
+	
+	public Viechle() {
+		System.out.println("inside 0-param cons veichle");
+	}
+	
+	public  void entertaiment(){
+		System.out.println("Viechle:: Audio Player based Entertainment");
+	}
+	public void blowHorn(){
+		System.out.println("Viechle:: Skoda Horn Sound");
+	}
+	/*public void move(){
+		System.out.println("Viechle:: move()");
+		ApplicationContext ctx=null;
+		Engine engg=null;
+		//create IOC container
+		ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		//get Depedent Bean class object	
+		engg=ctx.getBean(beanId,Engine.class);
+		//start Engine
+		engg.start();
+		System.out.println("Viechle Moved...");
+	}*/
+	
+	public void move(ApplicationContext ctx){
+		System.out.println("Viechle:: move()");
+		Engine engg=null;
+		//get Depedent Bean class object	
+		engg=ctx.getBean(beanId,Engine.class);
+		//start Engine
+		engg.start();
+		System.out.println("Viechle Moved...");
+	}
+
+}
